e415ed2e28f172713e81ba0f2e2032aa351d761e
MITRAIS\Muhammad_M <muhammad_m@mitrais.com>
2024-01-11 14:38:09 +0700

    Initial code

---
+++ AzureDevopsLearning/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AzureDevopsLearning.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
