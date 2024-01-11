b21fb76554d25a01cc4b93ae0458ebd1504aaec8
MITRAIS\Muhammad_M <muhammad_m@mitrais.com>
2024-01-11 15:32:59 +0700

    Change home controller

--- AzureDevopsLearning/Controllers/HomeController.cs
+++ AzureDevopsLearning/Controllers/HomeController.cs
@@ -1,11 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using AzureDevopsLearning.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using AzureDevopsLearning.Models;
+using System.Diagnostics;
 
 namespace AzureDevopsLearning.Controllers
 {
